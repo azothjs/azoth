320f791eb1803e29ca590e6396ee82db8f7ba6e8
marty <marty@martypdx.com>
2016-05-05 09:53:53 -0700

    update gobble and removed unused dependencies

--- gobblefile.js
+++ gobblefile.js
@@ -1,45 +1,37 @@
 const gobble = require( 'gobble' );
-const path = require( 'path' );
-const camelCase = require( 'camelCase' );
-
 const buble = require( 'rollup-plugin-buble' );
+const path = require( 'path' );
 
 
-// const bundleNpm = require( './bundle-npm' );
-// const package = require( './package.json' );
+const tests = gobble( 'test/tests' );
 
-const tests = gobble( 'test/tests' )
-const bad = /^test\./;
+const index = tests.include( '*.js' )
+	.transform( function importFiles( code ) {
+		const filename = this.filename;
+		const basename = path.basename( filename, path.extname( filename ) );	
+		return `import './${basename}';`;
+	})
+	.transform( 'concat', { 
+		dest: 'index.js', 
+		separator: '\n' 
+	});
 
-const index = tests.include( '*.js' ).transform( function( code ) {
-	const filename = this.filename;
-	const basename = path.basename( filename, path.extname( filename ) );
-	
-	if ( bad.test( basename ) ) return '';
-	
-	return `import './${basename}';`;
-	
-}).transform( 'concat', { 
-	dest: 'index.js', 
-	separator: '\n' 
-});
-
-
-
-const test = gobble( [ index, tests, 'src' ] ).transform( 'rollup', {
-	plugins: [ buble() ],
-	entry: 'index.js',
-	dest:  'test.js',
-	format: 'iife'
-});
+const test = gobble( [ index, tests, 'src' ] )
+	.transform( 'rollup', {
+		plugins: [ buble() ],
+		entry: 'index.js',
+		dest:  'test.js',
+		format: 'iife'
+	});
 
 
-const build = gobble( [ index, tests, 'src' ] ).transform( 'rollup', {
-	plugins: [ buble() ],
-	entry: 'main.js',
-	dest:  'diamond.js',
-	format: 'iife'
-});
+const build = gobble( [ index, tests, 'src' ] )
+	.transform( 'rollup', {
+		plugins: [ buble() ],
+		entry: 'main.js',
+		dest:  'diamond.js',
+		format: 'iife'
+	});
 
 const min = build.transform( 'uglifyjs', { ext: '.min.js' });
 
@@ -47,10 +39,4 @@ const min = build.transform( 'uglifyjs', { ext: '.min.js' });
 const html = gobble( 'test' ).include( 'index.html' );
 const research = gobble( 'research' ).moveTo( 'research' );
 
-// var modules = gobble( 'assets/npm' )
-// 	.transform( bundleNpm, {
-// 		modules: Object.keys( package.dependencies ),
-// 		dest: 'modules.js'
-// 	});
-
-module.exports = gobble( [ test, build, min, /*modules,*/ html, research ] );
+module.exports = gobble( [ test, build, min, html, research ] );
