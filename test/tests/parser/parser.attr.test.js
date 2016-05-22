30d54e93ea6b30ac2fc42955e32d00164b678e5a
marty <marty@martypdx.com>
2016-05-22 16:19:32 -0700

    refactor tests

--- test/tests/parser/parser.attr.test.js
+++ test/tests/parser/parser.attr.test.js
@@ -6,29 +6,41 @@ module( 'parser', () => {
 	module( 'attribute' );
 
 	test( 'basic attribute', t => {
+		
 		t.deepEqual( parser( '<div data-foo="{{bar}}">foo</div>' ), {
+			
 			html: '<div data-foo data-bind>foo</div>',
+			
 			defs: [
-				{ ref: 'bar', name: 'data-foo', binder: 'attribute' }
+				{ binder: 'attribute', ref: 'bar', name: 'data-foo'  }
 			]
 		});
+		
 	});
 	
 	test( 'attribute to value', t => {
+		
 		t.deepEqual( parser( '<input value="{{val}}">' ), {
+			
 			html: '<input value data-bind>',
+			
 			defs: [
-				{ ref: 'val', name: 'value', binder: 'property' }
+				{ binder: 'property', ref: 'val', name: 'value' }
 			]
 		});
+		
 	});
 	
 	test( 'attribute with non-bound attributes', t => {
+		
 		t.deepEqual( parser( '<span class="edit" data-done="{{done}}"></span>' ), {
+			
 			html: '<span class="edit" data-done data-bind></span>',
+			
 			defs: [
-				{ ref: 'done', name: 'data-done', binder: 'attribute' }
+				{ binder: 'attribute', ref: 'done', name: 'data-done' }
 			]
 		});
+		
 	});
 });
