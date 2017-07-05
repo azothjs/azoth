c51e58628dadc7391c59a76410f29564575e2912
martypdx <marty@martypdx.com>
2017-07-05 13:28:04 -0700

    allow recursive tests in folders

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,7 @@
 import './destructure.test.js';
 import './map-render.test.js';
-import './map.test.js';
 import './static-render.test.js';
 import './subscribe-render.test.js';
+import './operators/combine.test.js';
+import './operators/first.test.js';
+import './operators/map.test.js';
--- tests/make-index.js
+++ tests/make-index.js
@@ -1,13 +1,73 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readdir(__dirname, (err, files) => {
-    if(err) throw err;
-    const imports = files
-        .filter(f => /.test.js/.test(f))
-        .map(f => `import './${f}';${'\n'}`)
-        .join('');
-    fs.writeFile(path.join(__dirname, 'index.js'), imports, err => {
-        if(err) throw err;
+const testFile = /.test.js$/; 
+const jsFile = /.js$/;
+const isTestFile = f => testFile.test(f); 
+const isNotJsFile = f => !jsFile.test(f);
+
+make(__dirname)
+    .then(imports => {
+        writeFile(
+            path.join(__dirname, 'index.js'), 
+            imports.join('')
+        );
+    })
+    .catch(console.log);
+
+function make(dirname, imports = []) {
+    return readdir(dirname)
+        .then(files => {
+            imports.push(
+                ...files
+                    .filter(isTestFile)
+                    .map(f => path.join(dirname, f))
+                    .map(f => path.relative(__dirname, f))
+                    .map(f => `import './${f}';${'\n'}`)
+            );
+
+            return Promise.all(
+                files
+                    .filter(isNotJsFile)
+                    .map(f => path.join(dirname, f))
+                    .map(stat)
+            );
+        })
+        .then(files => {
+            const folders = files.filter(stat => stat.isDirectory());
+            return Promise.all(
+                folders.map(({ filePath }) => make(filePath, imports))
+            );
+        })
+        .then(() => imports);
+}
+
+function readdir(dirname) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(dirname, (err, files) => {
+            if(err) reject(err);
+            else resolve(files);
+        });
     });
-});
+}
+
+function stat(f) {
+    return new Promise((resolve, reject) => {
+        fs.stat(f, (err, stat) => {
+            if(err) reject(err);
+            else {
+                stat.filePath = f;
+                resolve(stat);
+            }
+        });
+    });
+}
+
+function writeFile(f, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(f, data, err => {
+            if(err) reject(err);
+            else resolve();
+        });
+    });
+}
