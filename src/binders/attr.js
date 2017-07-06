121df39b1be9cd01f7e3d6893183b8fdcd215767
martypdx <marty@martypdx.com>
2017-07-05 17:50:26 -0700

    give function correct name

--- src/binders/attr.js
+++ src/binders/attr.js
@@ -1,6 +1,6 @@
 const isProp = (name, node) => name in node;
 
-export default function textBinder(name) {
+export default function attrBinder(name) {
     return node => {
         return isProp(name, node)
             ? val => node[name] = val
