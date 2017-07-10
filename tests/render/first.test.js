87397fe269d64a7c0d95073497d0d05f2f786ccd
martypdx <marty@martypdx.com>
2017-07-09 17:53:48 -0700

    all new $ tests working

--- tests/render/first.test.js
+++ tests/render/first.test.js
@@ -20,7 +20,7 @@ module('first expression rendering', () => {
     });
 
     test('expression', t => {
-        const template = (x=$, y=$) => _`*${x} + *${y} = *${x + y}`;
+        const template = (x=$, y=$) => _`$${x} + $${y} = $${x + y}`;
         
         const x = new BehaviorSubject(5);
         const y = new BehaviorSubject(2);
@@ -30,21 +30,14 @@ module('first expression rendering', () => {
         t.equal(fixture.cleanHTML(), '5 + 2 = 7');
 
         x.next(3);
-        t.equal(fixture.cleanHTML(), '3 + 2 = 5');
         y.next(1);
-        t.equal(fixture.cleanHTML(), '3 + 1 = 4');
-
-        fragment.unsubscribe();
-
-        y.next(10);
-        t.equal(fixture.cleanHTML(), '3 + 1 = 4');
-
+        t.equal(fixture.cleanHTML(), '5 + 2 = 7');
     });
 
     test('conditional block with variables', t => {
         const yes = _`<span>Yes</span>`;
         const no = _`<span>No</span>`;
-        const template = (choice=$) => _`*${choice ? yes : no}#`;
+        const template = (choice=$) => _`$${choice ? yes : no}#`;
 
         const choice = new BehaviorSubject(true);
         const fragment = template(choice);
@@ -53,18 +46,13 @@ module('first expression rendering', () => {
         t.equal(fixture.cleanHTML(), '<span>Yes</span>');
         
         choice.next(false);
-        t.equal(fixture.cleanHTML(), '<span>No</span>');
-        
-        fragment.unsubscribe();
-        
-        choice.next(true);
-        t.equal(fixture.cleanHTML(), '<span>No</span>');
+        t.equal(fixture.cleanHTML(), '<span>Yes</span>');
     });
 
     test('block with array', t => {
         const template = (items=$) => _`
             <ul>
-                *${items.map(({ name }) => _`
+                $${items.map(({ name }) => _`
                     <li>${name}</li>	
                 `)}#
             </ul>
@@ -95,13 +83,11 @@ module('first expression rendering', () => {
 
         t.contentEqual(fixture.cleanHTML(), `
             <ul>
-                <li>goat</li>
-                <li>dragon</li>
-                <li>hot dog</li>
+                <li>balloon</li>
+                <li>hammer</li>
+                <li>lipstick</li>
             </ul>
         `);
-
-        fragment.unsubscribe();
     });
 
 
