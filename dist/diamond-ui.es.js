496293e5738f86c0ee578294ab6fa0e43c35cea0
martypdx <marty@martypdx.com>
2017-07-09 18:18:33 -0700

    update dist

--- dist/diamond-ui.es.js
+++ dist/diamond-ui.es.js
@@ -166,15 +166,38 @@ function __blockBinder( index ) {
         const top = document.createComment('block start');
         insertBefore(top, anchor);
         
+        let unsubscribe = null;
+        const callUnsubscribe = () => {
+            if(!unsubscribe) return;
+            
+            if(Array.isArray(unsubscribe)) {
+                for(let unsub of unsubscribe) unsub.unsubscribe();
+            } else {
+                unsubscribe.unsubscribe();
+            }
+        };
+        
         return val => {
             removePrior(top, anchor);
+            callUnsubscribe();
             const fragment = toFragment$1(val);
-            Array.isArray(fragment) ? fragment.forEach(f => insertBefore(f, anchor)) : insertBefore(fragment, anchor);
+            if(Array.isArray(fragment)) {
+                unsubscribe = [];
+                for(let f of fragment) {
+                    if(f.unsubscribe) unsubscribe.push(f.unsubscribe);
+                    insertBefore(f, anchor);
+                }
+            } else {
+                unsubscribe = fragment.unsubscribe || null;
+                insertBefore(fragment, anchor);
+            }
         };
     };
 }
 
 const toFragment$1 = val => typeof val === 'function' ? val() : val;
+
+// TODO: need to unsubscribe to prior fragment
 const removePrior = (top, anchor) => {
     let sibling = top.nextSibling;
     while(sibling && sibling !== anchor) {
