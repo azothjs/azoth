81ad150e7a983ce1ab7dbd0f4f5e947ee9db4034
martypdx <marty@martypdx.com>
2017-10-15 09:25:37 -0700

    preserve exact template (no missing tr, td) by using a template element to create fragment

--- src/dom.js
+++ src/dom.js
@@ -1,2 +1,8 @@
 const range = document.createRange();
 export const rawHtml = html => range.createContextualFragment(html);
+
+const template = document.createElement('template');
+export const makeTemplate = html => {
+    template.innerHTML = html;
+    return template.content;
+};
