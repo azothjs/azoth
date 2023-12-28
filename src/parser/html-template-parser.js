cdf1564455b6b01b6fc494b651d400f14f634d92
Marty Nelson <marty@martypdx.com>
2023-12-27 16:00:47 -0800

    more refactoring to classes

--- src/parser/html-template-parser.js
+++ src/parser/html-template-parser.js
@@ -8,14 +8,12 @@ const endQuote = /(?:=)\s*(["|'])\s*$/;
 
 export function getParser() {
 
-    const context = new TemplateContext();
-    const parser = new HtmlParser(context, { 
+    const templateContext = new TemplateContext();
+    const parser = new HtmlParser(templateContext, { 
         lowerCaseTags: false,
         lowerCaseAttributeNames: false,
         recognizeSelfClosing: true 
     });
-
-    const targets = [];
 
     // writeText needs to consider whether the prior .write()  
     // added quotation marks which now need to be removed
@@ -31,27 +29,16 @@ export function getParser() {
     function write(text) {
         writeText(text);
 
-        const { current, root } = context;
-        const { el } = current;
-        // queryIndex is the index of element in querySelectorAll bound els
-        let queryIndex = targets.lastIndexOf(el);
-        if(queryIndex === -1 && current !== root) queryIndex = (targets.push(el) - 1);
-
-        const binding = { queryIndex, element: el, };
-
+        const { element, root } = templateContext;
+       
+        const binding = templateContext.createBinding();
         /* property binder via attribute */
-        if(current.inTagOpen) { 
-            context.addPropertyBinder(binding);
-
+        if(binding.type === 'property') { 
             // Force the attribute to close properly by 
             // ensuring full set of matching quotes happen
             const match = text.match(endQuote) ?? '';
             const quote = match?.length > 1 ? match[1] : '""';
             parser.write(eatQuote = quote);
-        }
-        /* child node (text or block) */
-        else { 
-            context.addChildBinder(binding);
         }
     }
 
@@ -62,9 +49,8 @@ export function getParser() {
             parser.end();
             
             return {
-                bindings: context.bindings.map(({ 
-                    queryIndex, 
-                    element: { name, length }, 
+                bindings: templateContext.bindings.map(({ 
+                    element: { name, queryIndex, length }, 
                     property, 
                     childIndex 
                 }) => {
@@ -74,7 +60,7 @@ export function getParser() {
                 }),
                 // Originally placed into quasi chunks:
                 // quasis: chunks.map(chunk => chunk.flat().join(''))
-                html: context.html.flat().join('')
+                html: templateContext.html.flat().join('')
             };
         }
     };
@@ -83,68 +69,94 @@ export function getParser() {
 
 
 class ElementContext {
-    el = null;
     inTagOpen = true;
     attributes = [];
     bindings = [];
     waiting = null;
+    name = '';
+    keys = 0;
+    length = 0;
+    queryIndex = -1;
 
     constructor(name) {
-        this.el = {
-            name,
-            keys: 0,
-            length: 0
-        };
+        this.name = name;
     }
+}
+
+class Binding {
+    
 }
 
 class TemplateContext {
     html = [];
     stack = [];
     bindings = [];
-    
-    propertyBinding = null;
+    targets = [];
     root = null;
-    current = null;
+    
+    // context state
+    element = null;
+    propertyBinding = null;
 
     constructor() {
-        this.root = this.current = this.push('<>');
+        this.root = this.element = this.push('<>');
         this.root.inTagOpen = false;
     }
 
     push(name) {
         const ctx = new ElementContext(name);
-        this.stack.push(this.current = ctx);
+        this.stack.push(this.element = ctx);
         return ctx;
     }
 
-    addChildBinder(binding) {
+    createBinding() {
+        const { targets, element, root } = this;
+         // queryIndex is the index of element in querySelectorAll bound els
+        // let queryIndex = targets.lastIndexOf(element);
+        if(element.queryIndex === -1 && element !== root) {
+            element.queryIndex = targets.push(element) - 1;
+        }
+        const binding = { element };
+        element.bindings.push(binding);
+        
         this.bindings.push(binding);
-        this.current.bindings.push(binding);
-        const { el } = this.current;
-        // copy current value by assigning to binding index,
-        // el.length will increase as more children added
-        const index = binding.childIndex = el.length;
+
+        /* property binder via attribute */
+        if(element.inTagOpen) {
+            this.addPropertyBinder(binding);
+        }
+        else {
+            this.addChildBinder(binding);
+        }
+
+
+        return binding;
+    }
+
+    addChildBinder(binding) {
+        // copy element value by assigning to binding index,
+        // this.element.length will increase as more children added
+        binding.type = 'child';
+        const index = binding.childIndex = this.element.length;
         const replacement = `<!--child[${index}]-->`; // TODO: externalize replacement = i => `...`
         this.html.push(replacement);
-        el.length++;
+        this.element.length++;
     }
 
     addPropertyBinder(binding) {
-        this.bindings.push(binding);
-        this.current.bindings.push(binding);
-        const { current } = this;
-        binding.attributeIndex = current.attributes.length;
+        const { element } = this;
+        binding.attributeIndex = element.attributes.length;
+        binding.type = 'property';
         this.propertyBinding = binding;
     }
 
     pop(){
         this.stack.pop();
-        this.current = this.stack.at(-1);
+        this.element = this.stack.at(-1);
     }
 
     onopentagname(name) {
-        this.current.el.length++; // parent childNodes
+        this.element.length++; // parent childNodes
         this.push(name);
         this.html.push(`<${name}`);
     }
@@ -156,23 +168,23 @@ class TemplateContext {
 
         value ??= '';
         const isEmpty = !quote && !value;
-        this.current.attributes.push(isEmpty ? ` ${name}` : ` ${name}="${value}"`);
+        this.element.attributes.push(isEmpty ? ` ${name}` : ` ${name}="${value}"`);
     }
         
     onopentag() {    
-        const { current } = this;       
-        current.inTagOpen = false;
-        this.html.push(current.attributes); // open for further adds & removes
+        const { element } = this;       
+        element.inTagOpen = false;
+        this.html.push(element.attributes); // open for further adds & removes
         this.html.push('>');
     }
         
     ontext(text) {            
-        this.current.el.length++;
+        this.element.length++;
         this.html.push(text);
     }
         
     onclosetag(name, isImplied) {
-        const { current: { bindings, attributes } } = this;
+        const { element: { bindings, attributes } } = this;
         // void, self-closing, tags
         if(!voidElements.has(name)) this.html.push(`</${name}>`);
 
@@ -187,7 +199,7 @@ class TemplateContext {
     }
         
     oncomment(comment) {
-        this.current.el.length++;
+        this.element.length++;
         this.html.push(`<!--${comment}-->`);
     }
         
