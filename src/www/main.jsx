7e38ab73c725d694ccf852f7258a5377c6be81d6
Marty Nelson <marty@martypdx.com>
2024-01-23 12:27:01 -0800

    test app changes

--- src/www/main.jsx
+++ src/www/main.jsx
@@ -1,31 +1,69 @@
+import { fetchEmojis } from './fetchEmojis.js';
+import { multiplex } from './multiplex.js';
+import './RawHtml.jsx';
+// import { click, mouseup } from 'delegates';
 import './style.css';
 
-const $Header = <header>
-    <h1>Emojis for all my friends</h1>
-</header>;
+const start = new Date();
 
-const $emoji = ({ name }) => <li>{name}</li>;
-
-async function fetchEmojis() {
-    const res = await fetch('https://emojihub.yurace.pro/api/all');
-    return res.json();
+class RawHtml extends HTMLElement {
+    static observedAttributes = ['html'];
+    render() {
+        return <li>{this.name}</li>;
+    }
+    attributeChangedCallback(name, old, value) {
+        if(name === 'html') {
+            this.innerHTML = value;
+        }
+    }
 }
 
-const promise = fetchEmojis().then(emojis => <>{emojis.map($emoji)}</>);
+window.customElements.define('raw-html', RawHtml);
 
+function InnerHtml({ html, className = '' }) {
+    const rawEmoji = <span className={className ?? ''}></span>;
+    rawEmoji.firstChild.innerHTML = html;
+    return rawEmoji;
+}
 
-const $Emojis = <ul>{promise}</ul>;
+function EmojiList({ emojis }) {
+    return <ul>
+        {emojis.map(Emoji)}
+    </ul>;
+}
 
-document.body.append(
-    $Header,
-    $Emojis
+function Emoji({ name, unicode, htmlCode }) {   
+    return <li>
+        {InnerHtml({ html: htmlCode.join('') })} 
+        {name}
+        {unicode} 
+    </li>;
+}
+
+function EmojiCount({ count }) {
+    return <span>{count}</span>;
+}
+
+const [Count, List] = multiplex(
+    fetchEmojis(), 
+    ({ length }) => EmojiCount({ count: length }), 
+    emojis => EmojiList({ emojis }),
 );
 
+const App = <div>
+    <header>
+        <h1>{Count} emojis for all my friends</h1>
+    </header>
+        
+    <main>
+        <h2>Amazing Emoji List</h2>
+        {List}
+    </main>
+
+</div>;
+
+document.body.append(App);
 
 
-// const $App = <>
-//     {$Header}
-//     <main>
-//         {$Emojis}
-//     </main>
-// </>
+
+
