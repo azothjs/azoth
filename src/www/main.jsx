776444a3fad059a67a46a9da289538f5d8e426d8
Marty Nelson <marty@martypdx.com>
2024-01-22 23:17:29 -0800

    async layout management

--- src/www/main.jsx
+++ src/www/main.jsx
@@ -1,37 +1,70 @@
 import { multiplex } from './multiplex.js';
 import './style.css';
 
-const { resolve, promise } = Promise.withResolvers();
-setTimeout(resolve, 3000);
+const EMOJIS = 'EMOJIS';
 
 async function fetchEmojis() {
-    await promise;
+    const json = localStorage.getItem(EMOJIS);
+    if(json) {
+        try {
+            return JSON.parse(json);
+        }
+        catch(ex) {
+            // failed parse
+            localStorage.removeItem(EMOJIS);
+        }
+    }
+    // await sleep(3000);
     const res = await fetch('https://emojihub.yurace.pro/api/all');
-    return res.json();
+    const emojis = await res.json();
+
+    localStorage.setItem(EMOJIS, JSON.stringify(emojis, true, 4));
+    
+    return emojis;
 }
+const start = new Date();
+
+class RawHtml extends HTMLElement {
+    static observedAttributes = ['html'];
+    render() {
+        return <li>{this.name}</li>;
+    }
+    attributeChangedCallback(name, old, value) {
+        if(name === 'html') {
+            this.innerHTML = value;
+        }
+    }
+}
+window.customElements.define('raw-html', RawHtml);
 
 const $emojiCount = ({ length }) => <span>{length}</span>;
-const $emoji = ({ name }) => <li>{name}</li>;
+const $emoji = ({ name, unicode, htmlCode }) => {    
+    // const rawEmoji = <span class="emoji"></span>;
+    // rawEmoji.firstChild.innerHTML = htmlCode.join('');
+    return <li><span is="raw-html" html={htmlCode} /> {unicode} {name}</li>;
+};
 const $emojiList = emojis => <ul>{emojis.map($emoji)}</ul>;
 
-const Emojis = multiplex(fetchEmojis(), {
-    Count: $emojiCount, 
-    List: $emojiList,
-});
+const [Count, List] = multiplex(
+    fetchEmojis(), 
+    $emojiCount, 
+    $emojiList,
+);
 
 const App = <div>
     <header>
-        <h1>{Emojis.Count} Emojis for all my friends</h1>
+        <h1>{Count} Emojis for all my friends</h1>
     </header>
         
     <main>
         <h2>Amazing Emoji List</h2>
-        {Emojis.List}
+        {List}
     </main>
 
 </div>;
 
 document.body.append(App);
+
 
 
 
--- src/www/multiplex.js
+++ src/www/multiplex.js
@@ -1,4 +1,26 @@
-export function multiplex(promise, outlets) {
+
+
+
+export function multiplex(promise, ...outlets) {
+    const list = outlets.map(handler => {
+        const { promise, resolve, reject } = Promise.withResolvers();
+        return { promise, resolve, reject, handler };
+    });
+
+    dispatchAsync(promise, list);
+
+    return list.map(({ promise }) => promise);
+}
+
+async function dispatchAsync(promise, list) {
+    promise.then(data => {
+        list.forEach(({ resolve, handler }) => {
+            resolve(handler(data));
+        });
+    });
+}
+
+export function keyedMultiplex(promise, outlets) {
     const list = Object.entries(outlets).map(([key, handler]) => {
         const { promise, resolve, reject } = Promise.withResolvers();
         return { entry: [key, promise], resolve, reject, handler };
