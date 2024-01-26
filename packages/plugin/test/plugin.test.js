e74f2e22749c5dde641ed1c7ec9e8997b6915de6
Marty Nelson <marty@martypdx.com>
2024-01-26 15:22:25 -0800

    plugin package has working test

--- packages/plugin/test/plugin.test.js
+++ packages/plugin/test/plugin.test.js
@@ -3,8 +3,9 @@ import fs from 'node:fs';
 import { resolve } from 'node:path'
 
 test('plugin output produces same snapshot', async ({ expect }) => {
-    const path = resolve(__dirname, '../out/compiled.js')
-    expect(path).toMatchInlineSnapshot(`"/Users/marty/gitmllc/azoth/packages/plugin/out/compiled.js"`);
+    const expected = resolve(__dirname, './plugin.expected.js')
+    const path = resolve(__dirname, './out/compiled.js')
+    expect(path).toMatchInlineSnapshot(`"/Users/marty/gitmllc/azoth/packages/plugin/test/out/compiled.js"`);
     const output = fs.readFileSync(path, 'utf8');
-    expect(output).toMatchFileSnapshot
+    expect(output).toMatchFileSnapshot(expected)
 })
