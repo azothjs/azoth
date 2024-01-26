eb4afdc2a41acd30dba5be28fce555a6ee99d4f7
Marty Nelson <marty@martypdx.com>
2024-01-26 15:24:05 -0800

    basic plugin package

--- packages/vite-plugin-azoth/test/plugin.test.js
+++ packages/vite-plugin-azoth/test/plugin.test.js
@@ -5,7 +5,6 @@ import { resolve } from 'node:path'
 test('plugin output produces same snapshot', async ({ expect }) => {
     const expected = resolve(__dirname, './plugin.expected.js')
     const path = resolve(__dirname, './out/compiled.js')
-    expect(path).toMatchInlineSnapshot(`"/Users/marty/gitmllc/azoth/packages/plugin/test/out/compiled.js"`);
     const output = fs.readFileSync(path, 'utf8');
     expect(output).toMatchFileSnapshot(expected)
 })
