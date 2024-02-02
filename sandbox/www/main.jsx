c2803c90887cba48a55599c20118dbaf02ce978c
Marty Nelson <marty@martypdx.com>
2024-02-01 17:10:00 -0800

    normal sandbox emoji app

--- sandbox/www/main.jsx
+++ sandbox/www/main.jsx
@@ -1,5 +1,5 @@
-//import $App from './App';
-//import './style.css';
-import './create-1000-rows.jsx';
+import $App from './App';
+import './style.css';
+// import './create-1000-rows.jsx';
 
-// document.body.append($App);
+document.body.append($App);
