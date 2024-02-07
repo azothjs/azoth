72bb21ff71114747488f918bd8c847391ffdd2a3
Marty Nelson <marty@martypdx.com>
2024-02-07 15:50:00 -0800

    revert to standard approach that might work now

--- packages/vite-plugin-azoth/vite.config.js
+++ packages/vite-plugin-azoth/vite.config.js
@@ -3,23 +3,23 @@ import { resolve } from 'path';
 
 export default defineConfig({
     exclude: ['./out'],
-    build: {
-        lib: {
-            // Could also be a dictionary or array of multiple entry points
-            // eslint-disable-next-line no-undef
-            entry: resolve(__dirname, 'index.js'),
-            name: 'VitePluginAzoth',
-            formats: ['es'],
-            // the proper extensions will be added
-            fileName: 'vite-plugin-azoth',
-        },
-        rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: [
-                '@rollup/pluginutils',
-                '@azothjsx/compiler',
-            ],
-        },
-    },
+    // build: {
+    //     lib: {
+    //         // Could also be a dictionary or array of multiple entry points
+    //         // eslint-disable-next-line no-undef
+    //         entry: resolve(__dirname, 'index.js'),
+    //         name: 'VitePluginAzoth',
+    //         formats: ['es'],
+    //         // the proper extensions will be added
+    //         fileName: 'vite-plugin-azoth',
+    //     },
+    //     rollupOptions: {
+    //         // make sure to externalize deps that shouldn't be bundled
+    //         // into your library
+    //         external: [
+    //             '@rollup/pluginutils',
+    //             '@azothjsx/compiler',
+    //         ],
+    //     },
+    // },
 });
