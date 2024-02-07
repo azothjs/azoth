42fbaf5f187e76aaebba5f3a2e0f2950d7b5bc35
Marty Nelson <marty@martypdx.com>
2024-02-07 12:02:02 -0800

    don't include test files

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import inspect from 'vite-plugin-inspect';
 // import AzothPlugin from './packages/vite-plugin-azoth/vite-azoth-plugin.js';
 
 export default defineConfig({
-    // optimizeDeps: {
-    //     esbuildOptions: {
-    //         jsx: 'preserve',
-    //     },
-    // },
-    // esbuild: {
-    //     jsx: 'preserve',
-    // },
     test: {
         // includeSource: ['src/**/*.{js,ts}'],
         // update: true,
