6d78dbd0ab37c295b5fd8bcc01edea13597f2c39
Marty Nelson <marty@martypdx.com>
2024-02-07 13:05:30 -0800

    try and include compiler in plugin

--- packages/vite-plugin-azoth/vite.config.js
+++ packages/vite-plugin-azoth/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    exclude: ['./out']
+    exclude: ['./out'],
+    optimizeDeps: {
+        include: ['@azothjsx/compiler'],
+    },
 });
