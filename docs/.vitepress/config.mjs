80fe0f84c7921f83a94f8595b38cc4dae90d10c9
Marty Nelson <marty@martypdx.com>
2024-02-08 09:27:20 -0800

    basic vitepress site

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+    title: "Azoth JSX",
+    description: "Azoth JSX UI library documentation",
+    head: [['link', { rel: 'icon', href: '/azothjsx-ico.png' }]],
+    themeConfig: {
+        logo: '/azothjsx-ico.png',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Examples', link: '/markdown-examples' }
+        ],
+
+        sidebar: [
+            {
+                text: 'Examples',
+                items: [
+                    { text: 'Markdown Examples', link: '/markdown-examples' },
+                    { text: 'Runtime API Examples', link: '/api-examples' }
+                ]
+            }
+        ],
+
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/azothjsx/azoth' }
+        ]
+    }
+})
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Azoth JSX"
+#   text: "Hypermedia DOM libarary for the web platform"
+  tagline: A JSX UI library for building hypermedia apps on the web platform
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
