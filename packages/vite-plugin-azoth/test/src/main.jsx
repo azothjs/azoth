14f9e5253623b27b71543921743377f963f40184
Marty Nelson <marty@martypdx.com>
2024-02-10 09:09:43 -0800

    remove non-core imports

--- packages/vite-plugin-azoth/test/src/main.jsx
+++ packages/vite-plugin-azoth/test/src/main.jsx
@@ -1,42 +1,13 @@
 import { fetchEmojis } from './fetchEmojis.js';
-import { branch } from '@azothjsx/azoth/futures';
 import './style.css';
 
-function InnerHtml({ html, className = '' }) {
-    const rawEmoji = <span className={className ?? ''}></span>;
-    rawEmoji.firstChild.innerHTML = html;
-    return rawEmoji;
-}
-
-function EmojiList({ emojis }) {
-    return <ul>
-        {emojis.map(Emoji)}
-    </ul>;
-}
-
-function Emoji({ name, unicode, htmlCode }) {   
-    return <li>
-        {InnerHtml({ html: htmlCode.join('') })} 
-        {name}
-        {unicode} 
-    </li>;
-}
-
-function EmojiCount({ count }) {
-    return <span>{count}</span>;
-}
-
-const [Count, List] = branch(
-    fetchEmojis(), 
-    ({ length }) => EmojiCount({ count: length }), 
-    emojis => EmojiList({ emojis }),
-);
+const List = fetchEmojis().then(emojis => EmojiList({ emojis }));
 
 const App = <>
     <header>
-        <h1>{Count} emojis for all my friends</h1>
+        <h1>Emojis for all my friends</h1>
     </header>
-        
+
     <main>
         <h2>Amazing Emoji List</h2>
         {List}
@@ -44,6 +15,26 @@ const App = <>
 </>;
 
 document.body.append(App);
+
+function EmojiList({ emojis }) {
+    return <ul>
+        {emojis.map(Emoji)}
+    </ul>;
+}
+
+function Emoji({ name, unicode, htmlCode }) {
+    return <li>
+        {InnerHtml({ html: htmlCode.join('') })}
+        {name}
+        {unicode}
+    </li>;
+}
+
+function InnerHtml({ html, className = '' }) {
+    const rawEmoji = <span className={className ?? ''}></span>;
+    rawEmoji.firstChild.innerHTML = html;
+    return rawEmoji;
+}
 
 
 
