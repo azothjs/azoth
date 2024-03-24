c5d08d2960e76716a0c2c1657d148aea8ebcb22e
Marty Nelson <marty@martypdx.com>
2024-03-24 06:34:37 -0700

    add optional initialAction param to reduce

--- packages/channels/reduce.js
+++ packages/channels/reduce.js
@@ -2,11 +2,12 @@ import { Sync } from '../maya/compose/compose.js';
 import { generator } from './generator.js';
 import { TransformNotFunctionArgumentError } from './throw.js';
 
-export function reduce(reducer, init) {
+export function reduce(reducer, init, initialAction = null) {
     if(reducer && typeof reducer !== 'function') {
         throw new TransformNotFunctionArgumentError(reducer, { method: 'reduce', param: 'reducer' });
     }
-    let state = reducer(init);
+
+    let state = reducer(init, initialAction);
     const [iter, dispatch] = generator(action => state = reducer(state, action));
     return [Sync.wrap(state, iter), dispatch];
 }
