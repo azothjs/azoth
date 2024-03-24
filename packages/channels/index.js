4c655034efdb9ec9486e4a7d6621dd63e2268afa
Marty Nelson <marty@martypdx.com>
2024-03-23 19:17:27 -0700

    fix exports and reimports

--- packages/channels/index.js
+++ packages/channels/index.js
@@ -1,5 +1,8 @@
 import './with-resolvers-polyfill.js';
-export { use } from './consume.js';
+export { branch } from './branch.js';
+export { channel } from './channel.js';
 export { consume } from './consume.js';
-export { repeat } from './tee.js';
-export { observe } from './unicast.js';
+export { generator } from './generator.js';
+export { reduce } from './reduce.js';
+export { tee } from './tee.js';
+export { unicast } from './unicast.js';
